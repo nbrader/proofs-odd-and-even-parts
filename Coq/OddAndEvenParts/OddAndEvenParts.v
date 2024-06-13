b02eaf63123679532b23685a6b5482bc1958c714
Nathan Brader <nbrader@gmail.com>
2024-06-13 16:52:38 +0100

    Replaced use of lra with a more explicit derivation.

--- Coq/OddAndEvenParts/OddAndEvenParts.v
+++ Coq/OddAndEvenParts/OddAndEvenParts.v
@@ -5,8 +5,8 @@ Open Scope R_scope.
 
 Require Import Psatz.
 
-Definition evenPart : (R -> R) -> (R -> R) := fun f => fun x => (f x + f (-x)) * 0.5.
-Definition oddPart  : (R -> R) -> (R -> R) := fun f => fun x => (f x - f (-x)) * 0.5.
+Definition evenPart : (R -> R) -> (R -> R) := fun f => fun x => (f x + f (-x)) * /2.
+Definition oddPart  : (R -> R) -> (R -> R) := fun f => fun x => (f x - f (-x)) * /2.
 
 Definition isEven (f : R -> R) := forall (x : R), f (-x) =   (f x).
 Definition isOdd  (f : R -> R) := forall (x : R), f (-x) = - (f x).
@@ -29,16 +29,57 @@ Proof.
   intros.
   unfold oddPart.
   rewrite Ropp_involutive with (r := x).
-  rewrite Rmult_minus_distr_r with (r1 := 0.5) (r2 := f x) (r3 := f (-x)).
-  rewrite Ropp_minus_distr with (r1 := f x * 0.5) (r2 := f (-x) * 0.5).
-  rewrite <- Rmult_minus_distr_r with (r1 := 0.5) (r2 := f (-x)) (r3 := f x).
+  rewrite Rmult_minus_distr_r with (r1 := /2) (r2 := f x) (r3 := f (-x)).
+  rewrite Ropp_minus_distr with (r1 := f x * /2) (r2 := f (-x) * /2).
+  rewrite <- Rmult_minus_distr_r with (r1 := /2) (r2 := f (-x)) (r3 := f x).
   reflexivity.
 Qed.
 
 Theorem fIsEvenPlusOdd : forall (f : R -> R) (x : R), f x = evenPart f x + oddPart f x.
 Proof.
   intros.
+  (*
+    f x = evenPart f x + oddPart f x
+  *)
+
   unfold evenPart.
   unfold oddPart.
-  lra.
+  (*
+    f x = (f x + f (- x)) * /2 + (f x - f (- x)) * /2
+  *)
+
+  rewrite Rmult_plus_distr_r.
+  rewrite Rmult_minus_distr_r.
+  (*
+    f x = f x * /2 + f (- x) * /2 + (f x * /2 - f (- x) * /2)
+    f x = (f x * /2 + f (- x) * /2) + (f x * /2 - f (- x) * /2)
+  *)
+
+  unfold Rminus.
+  (*
+    f x = f x * /2 + f (- x) * /2 + (f x * /2 + - (f (- x) * /2))
+  *)
+
+  rewrite <- Rplus_assoc with (r1 := f x * /2 + f (- x) * /2) (r2 := f x * /2) (r3 := - (f (- x) * /2)).
+  rewrite Rplus_assoc with (r1 := f x * /2) (r2 := f (- x) * /2) (r3 := f x * /2).
+  rewrite Rplus_comm with (r1 := f (- x) * /2) (r2 := f x * /2).
+  rewrite Rplus_assoc.
+  rewrite Rplus_assoc.
+  rewrite Rplus_opp_r.
+  rewrite Rplus_0_r.
+  (*
+    f x = f x * / 2 + f x * / 2
+  *)
+
+  rewrite <- Rmult_plus_distr_l with (r1 := f x) (r2 := /2).
+  rewrite <- Rmult_1_l with (r := /2).
+  rewrite <- Rmult_plus_distr_r.
+  replace (1+1) with 2 by reflexivity.
+  rewrite Rinv_r by (apply Rgt_not_eq; apply Rlt_gt; apply Rlt_0_2).
+  rewrite Rmult_1_r.
+  (*
+    f x = f x
+  *)
+
+  reflexivity.
 Qed.
